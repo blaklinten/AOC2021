0e24c723bef6932f2625e3b3a4f848d630b35106
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2021-12-01 20:53:55 +0100

    Bash: solution part 2, day 1

--- day01/solution.sh
+++ day01/solution.sh
@@ -2,44 +2,53 @@
 
 INPUT="input.txt"
 TEST="test_input.txt"
-is_current_bigger()
-{
+
+is_current_bigger() {
     [ "$1" -gt "$2" ] && echo "yes" || echo "no"
 }
 
-part_1()
-{
+part_1() {
     echo "Part 1"
     previous="$(head -n1 $1)"
 
     while IFS= read -r number || [ -n "$number" ]; do
-        [ "$(is_current_bigger $number $previous)" = "yes" ] && {
-            total=$((total + 1))
-        }
+        [ "$(is_current_bigger $number $previous)" = "yes" ] && total=$((total + 1))
         previous="$number"
     done < "$1"
     echo "$total"
 }
 
-part_2()
-{
+part_2() {
     echo "Part 2"
-    index=0
+
     total=0
-
     while IFS= read -r number || [ -n "$number" ]; do
-        is_not_prime "$number"
-        if [ "$?" = 1 ]; then
-            [ "$((index % 2))" = 0 ] && total="$((total + number))" || total="$((total - number))"
-        fi
 
-        index="$((index + 1))"
+        [ -z "$second_offset" ] && {
+            second_offset="$first_offset"
+            first_offset="$number"
+            continue
+        }
+
+        current_window=$((second_offset + first_offset + number))
+
+        [ -z "$previous_window" ] && {
+            previous_window="$current_window"
+            second_offset="$first_offset"
+            first_offset="$number"
+            continue
+        }
+
+        [ "$(is_current_bigger $current_window $previous_window)" = "yes" ] && total=$((total + 1))
+        
+        second_offset="$first_offset"
+        first_offset="$number"
+        previous_window="$current_window"
     done < "$1"
     echo "$total"
 }
 
-solve()
-{
+solve() {
     [ "$part" = "part1" ] && part_1 "$1" || part_2 "$1"
 }
- solve "$INPUT"
+solve "$INPUT"
