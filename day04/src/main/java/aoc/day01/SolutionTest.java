9b6ac9530ed8cc6cf762d37bf5a66cc2f48d9651
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2021-12-08 18:03:09 +0100

    WIP - day04

---
+++ day04/src/main/java/aoc/day01/SolutionTest.java
@@ -0,0 +1,47 @@
+package aoc.day01;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+  Solution solution = new Solution();
+  public static List<String> testInput = new ArrayList<>();
+  int[] count;
+  boolean oxygen = true;
+  boolean co2 = false;
+
+  @BeforeEach
+  public void init() {
+    testInput.clear();
+    testInput.add("11001");
+    testInput.add("10001");
+    testInput.add("00011");
+    testInput.add("01111");
+  }
+
+  /*@Test
+  public void filterTest() {
+    List<String> res = solution.filter(testInput, 1, 1);
+    assertEquals(res.size(), 2);
+    assertEquals(res.get(0), testInput.get(0));
+    assertEquals(res.get(1), testInput.get(1));
+  }
+
+  @Test
+  public void getRatingTest_oxygen(){
+    String oxygenResult = solution.getRating(solution.toBinary(testInput), oxygen);
+    assertEquals("11001", oxygenResult);
+  }
+
+  @Test
+  public void getRatingTest_co2(){
+    String co2Result = solution.getRating(solution.toBinary(testInput), co2);
+    assertEquals("00011", co2Result);
+  }*/
+}
