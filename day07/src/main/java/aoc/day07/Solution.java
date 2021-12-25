a2cdb42594be9fb5a2deb1b38e9bd5d8efa615aa
Lucas Nilsson <lucas@fripost.org>
2021-12-25 13:35:46 +0100

    Day07, part2

--- day07/src/main/java/aoc/day07/Solution.java
+++ day07/src/main/java/aoc/day07/Solution.java
@@ -11,17 +11,19 @@ public class Solution {
 
   public static void main(String[] args) {
     Solution solution = new Solution();
-    solution.solve("test_input.txt");
+    solution.solve("input.txt");
   }
 
   void solve(String input) {
     try {
       List<String> lines = Files.readAllLines(Path.of(input));
+      List<String> positions = Arrays.asList(lines.get(0).split(","));
+
       String part = System.getenv("part");
       if (part.equals("part1")) {
-        part1(Arrays.asList(lines.get(0).split(",")));
+        part1(positions);
       } else if (part.equals("part2")) {
-        part2(Arrays.asList(lines.get(0).split(",")));
+        part2(positions);
       }
     } catch (IOException e) {
       System.err.println("Could not read from file \"" + input + "\"");
@@ -44,5 +46,22 @@ public class Solution {
 
   void part2(List<String> input) {
     System.out.println("part2");
+
+    List<Integer> sorted =
+        input.stream().map(Integer::parseInt).sorted().collect(Collectors.toList());
+
+    double meanValue = Math.ceil(sorted.stream().reduce(0, (acc, next) -> acc + next)/(double)sorted.size());
+
+    int totalCost =
+        sorted.stream().reduce(0, (acc, next) -> acc + calculateCost(Math.abs(next - meanValue)));
+    System.out.println("Total cost is: " + totalCost);
+  }
+
+  int calculateCost(double steps){
+      int cost = 0;
+      for (int i = 1; i <= steps; i++){
+          cost = cost + i;
+      }
+      return cost;
   }
 }
