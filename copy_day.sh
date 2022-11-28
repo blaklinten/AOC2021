d16bd7fe92e7901662c2f7139f119076fdc92d47
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2022-11-28 07:39:28 +0100

    Fix my broken copy script

--- copy_day.sh
+++ copy_day.sh
@@ -1,23 +1,23 @@
-#/bin/sh
+#!/bin/bash
+
+[ -z "$1" ] && {
+    echo "Specify which day to copy from, like so: \`copy_day.sh 04\`"
+    exit 1
+}
 
 DAY_TO_COPY="day$1"
 CURRENT_DAY="${PWD##*/}"
 
-[ -z "$DAY_TO_COPY" ] && {
-    echo "Specify which day to copy from, like so: `copy_day.sh 04`"
-    retrn -1
-}
-
 TARGET_DIR="$PWD"
 SOURCE_DIR="$PWD/../$DAY_TO_COPY"
 
-pushd "$SOURCE_DIR" >/dev/null
+pushd "$SOURCE_DIR" >/dev/null || exit
 
 for item in .project src {test_,}input.txt pom.xml; do
-    cp -r $item $TARGET_DIR
+    cp -r $item "$TARGET_DIR"
 done
  
-popd >/dev/null
+popd >/dev/null || exit
 
 find . -type f -exec sed -i -e s/$DAY_TO_COPY/$CURRENT_DAY/g '{}' \;
 
